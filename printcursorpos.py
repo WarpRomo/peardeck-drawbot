a6868bc35d46704f4d915ca15a4df51b3cec6079
WarPromo <kooltyme1@gmail.com>
2023-01-26 21:51:39 -0800

    Add files via upload

--- printcursorpos.py
+++ printcursorpos.py
@@ -1,13 +1,16 @@
 from ctypes import windll, Structure, c_long, byref
-import win32gui
+
 
 class POINT(Structure):
     _fields_ = [("x", c_long), ("y", c_long)]
 
 
-def MousePosition_win32gui():
-    flags, hcursor, (x,y) = win32gui.GetCursorInfo()
-    return (x,y)
+
+def queryMousePosition():
+    pt = POINT()
+    windll.user32.GetCursorPos(byref(pt))
+    return { "x": pt.x, "y": pt.y}
 
 while True:
-    print(MousePosition_win32gui());
+    pos = queryMousePosition()
+    print(pos)
